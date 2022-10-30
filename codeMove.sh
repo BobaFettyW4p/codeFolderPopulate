56b9406f620d69c2befd9ce9bd20d128c26c8cca
BobaFettyW4p <matthew.ivancic91@gmail.com>
2022-10-30 16:40:57 -0500

    updated file

--- codeMove.sh
+++ codeMove.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-codeDirectory = "$HOME/code"
-if [ test -f "$HOME/code" ]
-    echo "code directory already exists, moving on"
+test -f $HOME/code
+result=$?
+#checks for code directory; creates if not present
+if [ $result ]; then
+    echo "code directoy exists; moving on"
 else
     mkdir $HOME/code
 fi
+#loops over every file in the home directory; moves any with .git folder within it to the code folder
 
-for file in $HOME
+for file in $HOME/*;
     do
         echo "Processing $file"
-        if test -f "file/.git"
-            do
+        test -f $file/.git
+        test=$?
+        if [ $test ]; then
                 "Local repo found, moving to code..."
-                mv file ~/code
+                mv $file $HOME/code
         else
             echo "non-git repo, moving on"
         fi
